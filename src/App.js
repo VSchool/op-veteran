0fd18ae2c974f7ce9c04d5059869758673e3febe
Michael Stedman <michaelstedman81@gmail.com>
2021-02-13 16:59:14 -0700

    Deleted
    Deleted unecessary files from the styled-comopnents-demo.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import { handleIncrement, handleDecrement } from './utils'
-import { CountContext } from './context/Counter'
-import { Display } from './components/Display'
-import { Button } from './components/Button'
 
 const AppContainer = styled.div`
   width: 100%;
@@ -11,27 +7,11 @@ const AppContainer = styled.div`
   position: relative;
 `
 
-const ButtonWrapper = styled.div`
-  width: 200px;
-  height: 88px;
-  position: relative;
-  top: 48px;
-  left: calc(50% - 200px/2);
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`
-
 export default function App() {
-  const [count, setCount] = useContext(CountContext)
 
   return (
     <AppContainer>
-      <Display count={count} />
-      <ButtonWrapper>
-        <Button buttonText={'Increment'} buttonStyle={'primary'} onClick={() => handleIncrement(count, setCount)} />
-        <Button buttonText={'Decrement'} buttonStyle={'secondary'} onClick={() => handleDecrement(count, setCount)} />
-      </ButtonWrapper>
+      <h1>App Component</h1>
     </AppContainer>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './context/Counter'
 import App from './App';
 
 function Index() {
   return (
-    <Counter>
-      <App />
-    </Counter>
+    <App />
   )
 }
 
