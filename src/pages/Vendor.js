cabc2023d39649b05b0ac5ee4f4aac6908dcdf58
Michael Stedman <michaelstedman81@gmail.com>
2021-02-14 00:25:39 -0700

    Updated Header Component
    Added content and styling.

--- src/pages/Vendor.js
+++ src/pages/Vendor.js
@@ -1,18 +1,56 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Header } from '../components/Header'
 
 const VendorPageContainer = styled.div`
     box-sizing: border-box;
     width: 100%;
     height: 100%;
     position: relative;
-    border: 2px solid dodgerblue;
+    // border: 2px solid dodgerblue;
+
+    & > h1 {
+        box-sizing: border-box;
+        margin: 0px;
+        position: absolute;
+        top: 192px;
+        left: calc(50% - 328px/2);
+        width: 328px;
+        max-height: 64px;
+        // overflow: hidden;
+        font-family: Open Sans;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 32px;
+        line-height: 32px;
+        color: #545454;
+        // border: 1px solid dodgerblue;
+    }
+
+    & > .subtitle {
+        box-sizing: border-box;
+        margin: 0px;
+        position: absolute;
+        top: 272px;
+        left: calc(50% - 328px/2);
+        width: 328px;
+        max-height: 40px;
+        font-family: Open Sans;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 16px;
+        line-height: 20px;
+        color: #545454;
+        // border: 1px solid dodgerblue;
+    }
 `
 
 export default function Vendor() {
     return (
         <VendorPageContainer>
-            <p>Vendor Page</p>
+            <Header />
+            <h1>Vendor Page</h1>
+            <p className={'subtitle'}>Before selecting your booth, let's get some more point of contact information.</p>
         </VendorPageContainer>
     )
 }
