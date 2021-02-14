3873b1840bce85721d5fcdea4be1b32142a2b3ca
Michael Stedman <michaelstedman81@gmail.com>
2021-02-13 19:48:34 -0700

    Updated Input Component
    Added content; needs styling.

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,14 +1,55 @@
 import React from 'react'
 import styled from 'styled-components'
+import warningIcon from '../../assets/icons/warning-icon.svg'
 
 const InputContainer = styled.div`
-    border: 1px dotted dodgerblue;
+    width: 328px;
+    border: 1px solid black;
+
+    & > .label {
+        margin: 0px;
+        font-family: Open Sans;
+        font-style: normal;
+        font-weight: 300;
+        font-size: 12px;
+        line-height: 16px;
+        display: flex;
+        align-items: center;
+        letter-spacing: 0.02em;
+        color: #545454;
+        border: 1px dotted blue;
+    }
+
+    & > .helper-text {
+        margin: 0px;
+        font-family: Open Sans;
+        font-style: normal;
+        font-weight: 300;
+        font-size: 12px;
+        line-height: 16px;
+        display: flex;
+        align-items: center;
+        letter-spacing: 0.02em;
+        color: #EA7C7C;        
+        border: 1px dotted blue;
+    }
+
+    & > input {
+        // background: blue;
+    }
+
+    & > img {
+        border: 1px dotted blue;
+    }
 `
 
 export default function Input() {
     return (
         <InputContainer>
-            <p>Input</p>
+            <p className={'label'}>Input</p>
+            <input />
+            <img src={warningIcon} alt={'Invalid text entered'} />
+            <p className={'helper-text'}>{'Helper text'}</p>
         </InputContainer>
     )
 }
