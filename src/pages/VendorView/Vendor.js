dd814153c01955bc344d43071c0ab65057b75e04
Michael Stedman <michaelstedman81@gmail.com>
2021-02-17 22:03:03 -0700

    commit

--- src/pages/VendorView/Vendor.js
+++ src/pages/VendorView/Vendor.js
@@ -18,6 +18,7 @@ export default function Vendor() {
             <Header />
             {/* <RegistrationForm /> */}
             <SponsorshipSelection />
+        
         </VendorPageContainer>
     )
 }
