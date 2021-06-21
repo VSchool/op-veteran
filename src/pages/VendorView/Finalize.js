5db41e8bd6b36c5b694b419fb63495a1e9977ad0
Maira <masoomro16@ymail.com>
2021-06-21 11:43:15 -0700

    added

---
+++ src/pages/VendorView/Finalize.js
@@ -0,0 +1,14 @@
+import React, {useState, useContext} from 'react';
+import styled from 'styled-components'
+import { VendorContext } from '../../context/VendorProvider';
+import { BoothContext } from '../../context/BoothProvider';
+import { UserContext } from '../../context/UserProvider';
+
+const Finalize = (props) =>{
+    const {cart} = useContext(VendorContext)    
+    return (
+
+    )
+}
+
+export default Finalize
