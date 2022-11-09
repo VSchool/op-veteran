1c52f88c97a5940ba16387f949470bf5bd5c4356
matt wardlow <mwardlow0@gmail.com>
2022-11-08 21:44:34 -0500

    updated incomplete code

--- src/components/BoothCard/BoothCard.js
+++ src/components/BoothCard/BoothCard.js
@@ -115,14 +115,18 @@ const BoothCard = (props) => {
   }
 
   const isAllowed = () => {
+    const tier1 = ['Paladin', 'Stryker', 'Abrams', 'Bradley']
+    const tier2 = ['Paladin', 'Stryker']
+   
     if (vendor) return false
     if (restriction === 0) {
       return true
-    } else {
-      return false
+    } else if (restriction === 1) {
+      return tier1.some(tier => currentVendor.sponsorship.level.includes(tier))
+    } else if (restriction === 2) {
+      return tier2.some(tier => currentVendor.sponsorship.level.includes(tier))
     }
   }
-
   return (
     <CardContainer>
       <HeaderWrapper>
